d73adc55deb3fa43582cdbde0ae6785d7d48959d
DimaShabatyn <112180483+DimaShabatyn@users.noreply.github.com>
2023-06-11 16:34:42 +0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Container from './Container';
+import ContactForm from './ContactForm/ContactForm';
+import ContactsList from './ContactsList';
+import Filter from './Filter';
+import Loader from './Loader';
+import { MainTitle, SecondTitle } from './Container/Container.styled';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchContacts } from 'redux/operations';
+import { selectError, selectIsLoading } from 'redux/selectors';
+
+export const App = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  return (
+    <Container>
+      <MainTitle>Phonebook</MainTitle>
+      <ContactForm />
+      <SecondTitle>Contacts</SecondTitle>
+      <Filter />
+      {isLoading && !error && <Loader />}
+      <ContactsList />
+    </Container>
+  );
+};
+
---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from 'components/App';
+import './index.css';
+import { Provider } from 'react-redux';
+import store from 'redux/store';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { App } from './components/App';
+// import { ThemeProvider } from 'styled-components';
+// import { theme } from './components/theme';
+// import { Provider } from 'react-redux';
+// import { store, persistor } from './redux/store';
+// import { PersistGate } from 'redux-persist/integration/react';
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <PersistGate loading={null} persistor={persistor}>
+//         <ThemeProvider theme={theme}>
+//           <App />
+//         </ThemeProvider>
+//       </PersistGate>
+//     </Provider>
+//   </React.StrictMode>
+// );
